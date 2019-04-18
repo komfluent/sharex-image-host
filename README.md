3085505143fc4650c6a71c31864b8379022e9a25
Raven <marinus@reimer.world>
2019-04-18 17:40:42 +0200

    rave version

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # sharex-image-host
 Basic sharex image host written in js with express and express-uploader.
+
+## Usage
+### `POST /upload`
+Headers: `Authorization: YOUR_PASSWORD`
+
+Form-data: `image: THE_IMAGE`
+
+
+Response: 200
+```json
+{
+	"filename": "example.png"
+}
+```
+
+### `DELETE /:image`
+Headers: `Authorization: YOUR_PASSWORD`
+
+Response: 204
+
+## Configuration
+Config is stored in .env
+
+Example:
+
+```env
+PORT="1337" # port to run the application on
+IMAGE_AUTH="yourpassword" # password protection for image upload and deletion
+```
+
--- src/main.js
+++ src/main.js
@@ -3,16 +3,35 @@ const app = express();
 
 require('dotenv').config();
 
-const { existsSync, mkdirSync } = require('fs');
+const { existsSync, mkdirSync, writeFileSync, unlinkSync } = require('fs');
 const { join } = require('path');
 
+const port = parseInt(process.env.PORT) || 4000;
+
 const uploadDir = join(process.cwd(), 'images');
-if (!existsSync(uploadDir)) mkdirSync(uploadDir);
+if (!existsSync(uploadDir)) {
+	console.log('Created /images');
+	mkdirSync(uploadDir);
+}
 
-app.use(require('express-fileupload'));
-app.use('/images', express.static('images'));
+function handleImage(image, res) {
+	const extension = image.name.split('.')[1];
+	const id = new Date().getTime().toString(36) + '.' + extension;
+	writeFileSync(`./images/${id}`, image.data);
+	console.log(`+ ${id} (${image.name})`);
+	return { id };
+}
 
-app.post('/api/upload', (req, res) => {
+app.use(require('express-fileupload')());
+app.use('/', express.static('images'));
+
+app.get('/', (req, res) => {
+	return res
+		.status(404)
+		.send({ code: 404, message: 'Cannot GET /. Use POST /upload for image uploading.'})
+});
+
+app.post('/upload', (req, res) => {
 
 	if (req.headers.authorization !== process.env.IMAGE_AUTH) {
 		return res
@@ -21,11 +40,31 @@ app.post('/api/upload', (req, res) => {
 	}
 
 	const { image } = req.files;
-	const { id: filename, extension, error } = require('./handleImage')(image, res);
-	if (!error) return res.send({ filename, extension });
+	const { id: filename } = handleImage(image, res);
+	if (filename) return res.send({ filename });
+	return res
+		.status(500)
+		.send({ code: 500, message: 'Internal server error.' });
 });
 
-const port = parseInt(process.env.PORT) || 4000;
+app.delete('/:file', (req, res) => {
+
+	if (req.headers.authorization !== process.env.IMAGE_AUTH) {
+		return res
+			.status(401)
+			.send({ code: 401, message: 'Invalid authentication credentials!' });
+	}
+
+	const { file } = req.params;
+
+	unlinkSync(join(process.cwd(), 'images', file));
+	console.log(`- ${file}`);
+
+	return res
+		.status(204)
+		.send({ code: 204, message: `Successfully deleted ${file}.` })
+
+}) 
 
 app.listen(port, () => {
 	console.log(`Serving on port ${port}!`);
